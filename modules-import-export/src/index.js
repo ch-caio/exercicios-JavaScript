fb6123e849cc897386542ad6a530dec8d429df3c
Caio Henrique <ch.caio@hotmail.com>
2022-07-04 13:52:39 -0300

    novos exercicios

---
+++ modules-import-export/src/index.js
@@ -0,0 +1,3 @@
+import * as importar from './module'
+
+console.log(importar.nome, importar.sobrenome, importar.idade)
---
+++ modules-import-export/src/module.js
@@ -0,0 +1,9 @@
+export { sobrenome, idade }
+
+export const nome = 'caio';
+const sobrenome = 'pereira';
+const idade = 28;
+
+function soma(x, y) {
+    return x + y;
+}
