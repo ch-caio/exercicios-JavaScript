f269bc0e730852e5c34378dc4cf18471abf309a4
caio.pereira@foursys.com.br <73006651+ch-caio@users.noreply.github.com>
2022-10-21 17:21:20 -0300

    novos exercicios

---
+++ React/projeto-lista/src/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    })
+  }
+
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="Main">
+        <h1>Lista de tarefas</h1>
+        <h1>{novaTarefa}</h1>
+
+        <form action="">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit" >Enviar</button>
+        </form>
+      </div>
+    )
+  }
+}
