ad91a0cee0145ed25313218c894db2ad0182b1c9
caio.pereira@foursys.com.br <73006651+ch-caio@users.noreply.github.com>
2022-10-24 17:15:18 -0300

    novos exercicios

--- React/projeto-lista/src/components/Main.js
+++ React/projeto-lista/src/components/Main.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import './Main.css'
+import { FaPlus } from 'react-icons/fa';
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: []
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim;
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa]
+    });
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -12,17 +29,38 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+
+    const { novaTarefa, tarefas } = this.state;
 
     return (
-      <div className="Main">
+      <div className="main">
         <h1>Lista de tarefas</h1>
-        <h1>{novaTarefa}</h1>
 
-        <form action="">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit" >Enviar</button>
+        <form action="#" onSubmit={this.handleSubmit} className="formulario">
+
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+
+          <button type="submit" >
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              <h1>{tarefa}</h1>
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
+
       </div>
     )
   }
